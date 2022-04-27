0bff842283baef2de6d02cf7c290aab028067302
Calvin Vu <vucalvin95@gmail.com>
2022-04-26 20:49:46 -0700

    added smart pointer

--- lab1.cpp
+++ lab1.cpp
@@ -1,11 +1,14 @@
 // Calvin Vu
-// Lab 1: Morse Decryption
+// Lab 1: Bitsets
 // CIS29
+// C++17
 #include <fstream>
 #include <vector>
 #include <iostream>
 #include <algorithm>
 #include <tuple>
+#include <memory>
+#include <utility>
 using namespace std;
 
 class File{
@@ -13,7 +16,6 @@ class File{
         vector<char> rawdata;
     public:
         void readFile(string f);
-        string convert();
         vector<char> getData(){return rawdata;}
 };
 void File::readFile(string f){ 
@@ -42,51 +44,52 @@ class Database{
 
 class Process{
     private:
-        vector<tuple<char, string>> charList;
+        unique_ptr<vector<tuple<char, string>>> morseKey;
         string decrypted;
     public:
         Process(){
-            charList.push_back(make_tuple('A', "1001"));
-            charList.push_back(make_tuple('B', "01101010"));
-            charList.push_back(make_tuple('C', "01100110"));
-            charList.push_back(make_tuple('D', "011010"));
-            charList.push_back(make_tuple('E', "10"));
-            charList.push_back(make_tuple('F', "10100110"));
-            charList.push_back(make_tuple('G', "010110"));
-            charList.push_back(make_tuple('H', "10101010"));
-            charList.push_back(make_tuple('I', "1010"));
-            charList.push_back(make_tuple('J', "10010101"));
-            charList.push_back(make_tuple('K', "011001"));
-            charList.push_back(make_tuple('L', "10011010"));
-            charList.push_back(make_tuple('M', "0101"));
-            charList.push_back(make_tuple('N', "0110"));
-            charList.push_back(make_tuple('O', "010101"));
-            charList.push_back(make_tuple('P', "10010110"));
-            charList.push_back(make_tuple('Q', "01011001"));
-            charList.push_back(make_tuple('R', "100110"));
-            charList.push_back(make_tuple('S', "101010"));
-            charList.push_back(make_tuple('T', "01"));
-            charList.push_back(make_tuple('U', "101001"));
-            charList.push_back(make_tuple('V', "10101001"));
-            charList.push_back(make_tuple('W', "100101"));
-            charList.push_back(make_tuple('X', "01101001"));
-            charList.push_back(make_tuple('Y', "01100101"));
-            charList.push_back(make_tuple('Z', "01011010"));
-            charList.push_back(make_tuple('0', "0101010101"));
-            charList.push_back(make_tuple('1', "1001010101"));
-            charList.push_back(make_tuple('2', "1010010101"));
-            charList.push_back(make_tuple('3', "1010100101"));
-            charList.push_back(make_tuple('4', "1010101001"));
-            charList.push_back(make_tuple('5', "1010101010"));
-            charList.push_back(make_tuple('6', "0110101010"));
-            charList.push_back(make_tuple('7', "0101101010"));
-            charList.push_back(make_tuple('8', "0101011010"));
-            charList.push_back(make_tuple('9', "0101010110"));
-            charList.push_back(make_tuple('.', "100110011001"));
-            charList.push_back(make_tuple(',', "010110100101"));
-            charList.push_back(make_tuple('"', "100110100110"));
-            charList.push_back(make_tuple('\'', "100101010110"));
-            charList.push_back(make_tuple(' ', "11"));
+            morseKey = make_unique<vector<tuple<char, string>>>();
+            morseKey->push_back(make_tuple('A', "1001"));
+            morseKey->push_back(make_tuple('B', "01101010"));
+            morseKey->push_back(make_tuple('C', "01100110"));
+            morseKey->push_back(make_tuple('D', "011010"));
+            morseKey->push_back(make_tuple('E', "10"));
+            morseKey->push_back(make_tuple('F', "10100110"));
+            morseKey->push_back(make_tuple('G', "010110"));
+            morseKey->push_back(make_tuple('H', "10101010"));
+            morseKey->push_back(make_tuple('I', "1010"));
+            morseKey->push_back(make_tuple('J', "10010101"));
+            morseKey->push_back(make_tuple('K', "011001"));
+            morseKey->push_back(make_tuple('L', "10011010"));
+            morseKey->push_back(make_tuple('M', "0101"));
+            morseKey->push_back(make_tuple('N', "0110"));
+            morseKey->push_back(make_tuple('O', "010101"));
+            morseKey->push_back(make_tuple('P', "10010110"));
+            morseKey->push_back(make_tuple('Q', "01011001"));
+            morseKey->push_back(make_tuple('R', "100110"));
+            morseKey->push_back(make_tuple('S', "101010"));
+            morseKey->push_back(make_tuple('T', "01"));
+            morseKey->push_back(make_tuple('U', "101001"));
+            morseKey->push_back(make_tuple('V', "10101001"));
+            morseKey->push_back(make_tuple('W', "100101"));
+            morseKey->push_back(make_tuple('X', "01101001"));
+            morseKey->push_back(make_tuple('Y', "01100101"));
+            morseKey->push_back(make_tuple('Z', "01011010"));
+            morseKey->push_back(make_tuple('0', "0101010101"));
+            morseKey->push_back(make_tuple('1', "1001010101"));
+            morseKey->push_back(make_tuple('2', "1010010101"));
+            morseKey->push_back(make_tuple('3', "1010100101"));
+            morseKey->push_back(make_tuple('4', "1010101001"));
+            morseKey->push_back(make_tuple('5', "1010101010"));
+            morseKey->push_back(make_tuple('6', "0110101010"));
+            morseKey->push_back(make_tuple('7', "0101101010"));
+            morseKey->push_back(make_tuple('8', "0101011010"));
+            morseKey->push_back(make_tuple('9', "0101010110"));
+            morseKey->push_back(make_tuple('.', "100110011001"));
+            morseKey->push_back(make_tuple(',', "010110100101"));
+            morseKey->push_back(make_tuple('"', "100110100110"));
+            morseKey->push_back(make_tuple('\'',"100101010110"));
+            morseKey->push_back(make_tuple(' ', "11"));
         }
         void decrypt(Database& d);
         string get(){return decrypted;}
@@ -104,11 +107,11 @@ void Process::decrypt(Database& d){
         else if(temp == "00" || temp == "11"){
             if(temp == "11"){decrypted += " ";}
             else{
-                for (auto&& tuple: charList){
+                for (auto&& tuple: *morseKey){
                     char letter;
                     string binary;
                     tie(letter, binary) = tuple;
-                    if( binary == currentLetterBinary){decrypted += letter;}
+                    if(binary == currentLetterBinary){decrypted += letter;}
                 }
             }   
             currentLetterBinary = "";
@@ -119,7 +122,7 @@ void Process::decrypt(Database& d){
 }
 string Process::convert(vector<char> v){
     string binary = "";
-    for(int i = 0; i<v.size();i++){
+    for(int i=0; i<v.size();i++){
         binary.append(bitset<8>(v.at(i)).to_string());
     }
     return binary;
@@ -128,9 +131,7 @@ string Process::convert(vector<char> v){
 class Output{
     private:
     public:
-        void print(string s){
-            cout << "Decrypted Text: " << endl << s;
-        }
+        void print(string s){cout << "Decoded Text:\n" << s;}
 };
 
 int main(){
@@ -138,7 +139,7 @@ int main(){
     Database d;
     Process p;
     Output o;
-    f.readFile("morse.bin");
+    f.readFile("Morse.bin");
     d.setBinary(p.convert(f.getData()));
     p.decrypt(d);
     o.print(d.getResult());
